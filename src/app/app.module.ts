80a437ffd4883348dd6b99aa0354746c0c3eb149
Marina Pavlova <marina-fam@yandex.ru>
2018-11-13 21:22:24 +0300

    Add ivy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IvyComponent } from './ivy/ivy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IvyComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ivy/ivy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ivy',
+  templateUrl: './ivy.component.html',
+  styleUrls: ['./ivy.component.less']
+})
+export class IvyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
